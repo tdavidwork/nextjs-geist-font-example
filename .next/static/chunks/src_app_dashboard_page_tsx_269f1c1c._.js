507d82e999394213399d8b31dfcbb86cb6e5074f
tdavidwork <tdavid@bajau.com>
2025-07-17 17:42:45 +0700

    Add .next/static/chunks/src_app_dashboard_page_tsx_269f1c1c._.js 1752748965052

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_269f1c1c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b632e193._.js",
+  "static/chunks/node_modules_97f85cdb._.js"
+],
+    source: "dynamic"
+});
